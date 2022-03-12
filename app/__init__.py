e8177137c5ec0fa34d0b19854960e9ace2db49b6
sostenesapollo <sostenesapollo25@gmail.com>
2022-03-12 16:48:27 -0300

    fix: smtg fixed

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_migrate import Migrate
-from .model import configure as config_db
+from model import configure as config_db
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite////tmp/databse.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/cruzinho.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     config_db(app)
 
     Migrate(app, app.db)
-    
     return app
--- app/model.py
+++ app/model.py
@@ -1,12 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 def configure(app):
     db.init_app(app)
     app.db = db
 
-class Book(db.Model)::
+class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     book = db.Column(db.String(255))
     writer = db.Column(db.String(255))
