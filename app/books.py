36708b90b82f8f99262374b03d545d46d9d3cbf3
sostenesapollo <sostenesapollo25@gmail.com>
2022-03-12 18:18:08 -0300

    feat: post

--- app/books.py
+++ app/books.py
@@ -1,7 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, current_app, request
+from app.model import Book
+
+from app.serializer import BookSchema
 
 bp_books = Blueprint('books', __name__)
 
-@bp_books.route('/mostrar', methods=['GET'])
-def mostrar():
-    return 'ok'
+@bp_books.route('/books', methods=['GET'])
+def get():
+    bs = BookSchema(many=True)
+    result = Book.query.all()
+    return bs.jsonify(result), 200
+
+@bp_books.route('/books', methods=['POST'])
+def post():
+    # bs = BookSchema(many=True)
+    print(request.json['book'])
+    # result = Book.query.all()
+    return '---', 200
